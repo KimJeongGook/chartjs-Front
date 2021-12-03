8532d53b8e71294e6caab76fb35682e5729e4c2f
KimJengGook <gooki1210@naver.com>
2021-12-03 12:57:55 +0900

    LineChart 기본 구현

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 // import { Chart, registerables } from 'chart.js'
 // Chart.register(...registerables);
 // import Chart from 'chart.js/auto';
+
 import VerticalBarChart from './VerticalBarChart';
 import HorizontalBarChart from './HorizontalBarChart';
+import LineChart from './LineChart';
 
 function App() {
   return (
     <div>
       <VerticalBarChart />
       <HorizontalBarChart />
+      <LineChart />
     </div>
   );
 } 
