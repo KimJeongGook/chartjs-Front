d4e69a15c9e6d384a38701619af1c33578ed2db1
KimJengGook <gooki1210@naver.com>
2021-12-04 11:53:12 +0900

    chartjs-front

--- src/App.js
+++ src/App.js
@@ -14,23 +14,36 @@ import LineChart from './components/LineChart/LineChart';
 import Layout from './components/Layout';
 import MultiAxisLineChart from './components/LineChart/MultiAxisLineChart';
 import SteppedLineChart from './components/LineChart/SteppedLineChart';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const data = [50,40,30,35,40];
-  const labels = [2017,2018,2019,2020,2021];
+  const data = [50, 40, 30, 35, 40];
+  const labels = [2017, 2018, 2019, 2020, 2021];
+
+  useEffect(() => {
+    try {
+      const url = 'http://localhost:3001/csv'
+      const res = axios.get(url)
+      console.log(res)
+    } catch (e) {
+      console.log(e);
+    }
+  }, [])
+
   return (
     <Layout>
-      <VerticalBarChart data={data} labels={labels}/>
-      <HorizontalBarChart data={data} labels={labels}/>
-      <LineChart data={data} labels={labels}/>
-      <StackdBarChart data={data} labels={labels}/>
-      <StackdBarChartwithGroups data={data} labels={labels}/>
-      <FloatingBarChart data={data} labels={labels}/>
-      <BarChartBoarderRadius data={data} labels={labels}/>
-      <MultiAxisLineChart data={data} labels={labels}/>
-      <SteppedLineChart data={data} labels={labels}/>
+      <VerticalBarChart data={data} labels={labels} />
+      <HorizontalBarChart data={data} labels={labels} />
+      <LineChart data={data} labels={labels} />
+      <StackdBarChart data={data} labels={labels} />
+      <StackdBarChartwithGroups data={data} labels={labels} />
+      <FloatingBarChart data={data} labels={labels} />
+      <BarChartBoarderRadius data={data} labels={labels} />
+      <MultiAxisLineChart data={data} labels={labels} />
+      <SteppedLineChart data={data} labels={labels} />
     </Layout>
   );
-} 
+}
 
 export default App;
